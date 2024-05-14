90474d40829b9c7c1d2947834f2c2bb4f04ebc58
UrielEngelberg <165162205+UrielEngelberg@users.noreply.github.com>
2024-05-13 22:02:57 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,98 +1,76 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-
 using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace MyApp
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            int opcion;
-            const int  DIA1 = 45000, DIA2 =60000 , DIA3 = 30000, FULL_DIAS = 100000;
+            int opcion, dni, tipoEntrada, cantidad;
+            string nombre, apellido;
+            double fechaDeInscripcion;
+
             while (true)
             {
-                Console.WriteLine("Seleccione una opcion ");
-                Console.WriteLine("1 Nueva Inscripción 1 ");
-                Console.WriteLine("2 Obtener Estadísticas del Evento 2 ");
-                Console.WriteLine("3 Buscar Cliente 3 ");
-                Console.WriteLine("4 Cambiar entrada de un Cliente 4 ");
-                Console.WriteLine("5 Salir 5 ");
-            }
-            while (opcion <= 5)
-            {
-                 switch (opcion)
-            {
-                case 1 :
-                ingresaDias(opcion);
-                break;
-                case 2 :
+                Console.WriteLine("Seleccione una opción:");
+                Console.WriteLine("1. Nueva Inscripción");
+                Console.WriteLine("2. Obtener Estadísticas del Evento");
+                Console.WriteLine("3. Buscar Cliente");
+                Console.WriteLine("4. Cambiar entrada de un Cliente");
+                Console.WriteLine("5. Salir");
 
-                break;
-            }
-            }
-           
+                opcion = int.Parse(Console.ReadLine());
 
-        }
-        public static void ingresaDias(int opcion){
-            const int DIA1 = 45000, DIA2 = 60000 , DIA3 = 30000, FULL_DIAS = 100000;
-            int resultado = 0;
-            while (opcion > 4)
-            {
                 switch (opcion)
                 {
-                    case 1 :
-                    if (opcion == 1)
-                    {
-                        Console.WriteLine($"deberas abonar {DIA1} ");
-                    }
-                    else if (opcion == 1 && opcion == 2)
-                    {
-                        resultado = DIA1 + DIA2;
-                        Console.WriteLine($"deberas abonar {resultado}  ");
-                    }
-                    else if (opcion == 1 && opcion == 3)
-                    {
-                        resultado = DIA1 + DIA3;
-                        Console.WriteLine($"lo que debes abonar es {resultado} ");
-                    }
-                    break;
-                    case 2 : 
-                    if (opcion == 2)
-                    {
-                        Console.WriteLine($"Lo que debes abonar es {DIA2} ");
-                    }
+                    case 1:
+                        Console.WriteLine("Ingrese su nombre ");
+                        nombre = Console.ReadLine();
+                        Console.WriteLine("Ingrese su apellido ");
+                        apellido = Console.ReadLine();
+                        Console.WriteLine("Ingrese su DNI ");
+                        dni = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese la fecha de inscripción ");
+                        fechaDeInscripcion = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese el tipo de entrada (1, 2, 3 o 4) ");
+                        tipoEntrada = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese la cantidad de entradas ");
+                        cantidad = int.Parse(Console.ReadLine());
 
-                    else if (opcion == 2 && opcion == 3)
-                    {
-                        resultado = DIA2 + DIA3;
-                        Console.WriteLine($"lo que debes abonar es {resultado} ");
-                    }
-                    
-                    break;
-                    case 3 :
-                    if (opcion == 3)
-                    {
-                        Console.WriteLine($"lo que debes abonar es {DIA3} ");
-                    }
-                    break;
-                    case 4 :
-                    if (opcion == 4)
-                    {
-                        Console.WriteLine($"Lo que debes abonar es {FULL_DIAS} ");
-                    }
-                    break;
+                        int UltimoIDEntrada = Tiketera.AgregarCliente(nombre, apellido, dni, fechaDeInscripcion, tipoEntrada, cantidad);
+                        Console.WriteLine($"Se ha agregado el cliente con ID {UltimoIDEntrada} ");
+                        break;
+                    case 2:
+                        Tiketera.EstadisticasDelEvento();
+                        break;
+                    case 3:
+                        Console.Write("Ingrese el ID del cliente a buscar ");
+                        UltimoIDEntrada = int.Parse(Console.ReadLine());
+                        Tiketera.BuscarCliente(UltimoIDEntrada);
+                        break;
+                    case 4:
+                        Console.Write("Ingrese el ID del cliente a cambiar ");
+                        UltimoIDEntrada = int.Parse(Console.ReadLine());
+                        Cliente cliente = Tiketera.BuscarCliente(UltimoIDEntrada);
+
+                        if (cliente != null)
+                        {
+                            Console.Write("Ingrese el nuevo tipo de entrada (1, 2, 3 o 4): ");
+                            int nuevoTipoEntrada = int.Parse(Console.ReadLine());
+
+                            Tiketera.CambiarEntrada(cliente, nuevoTipoEntrada);
+                        }
+                        break;
+                    case 5:
+                        Console.WriteLine("Saliendo del programa ");
+                        return;
                     default:
-                    break;
-                }
-                if (opcion > 4)
-                {
-                    Console.WriteLine("no hay tantas opciones debe ser del 1 al 4 ");
+                        Console.WriteLine("Esta opcion no existe por favor seleccione una opción valida ");
+                        break;
                 }
             }
         }
     }
-    
 }
 
