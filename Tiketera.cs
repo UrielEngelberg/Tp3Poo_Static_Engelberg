fea80b836fd227461698042ad5f4f25e73d3f52d
UrielEngelberg <165162205+UrielEngelberg@users.noreply.github.com>
2024-05-13 22:04:43 -0300

    Update Tiketera.cs

--- Tiketera.cs
+++ Tiketera.cs
@@ -1,52 +1,85 @@
-public class tiketera{
-    private static int UltimoIDEntrada = 0;
+public class Tiketera
+    {
+        private static int UltimoIDEntrada = 0;
+        private static Dictionary<int, Cliente> DicCliente = new Dictionary<int, Cliente>();
 
-    private static Dictionary<int, Cliente> DicCliente = new Dictionary<int, Cliente>();
-
-    public static int devolverUltimoID(){
-        
-        return UltimoIDEntrada;
-    } 
-
-    
-    public static int agregarCliente(string nombre, string apellido,int dni,double fechaDeInscripcion,int tipoEntrada, int cantidad){
-        Cliente clienteCompleto = new Cliente(dni, nombre, apellido, fechaDeInscripcion,tipoEntrada,cantidad);
-        DicCliente.Add(UltimoIDEntrada,clienteCompleto);
-        return UltimoIDEntrada;
-    }
-
-    public static int buscarCliente(int UltimoIDEntrada){
-        if (DicCliente.ContainsKey(UltimoIDEntrada))
+        public static int DevolverUltimoID()
         {
-            
+            return UltimoIDEntrada;
         }
-        return UltimoIDEntrada;
-    }
 
-    public static bool CambiarEntrada(int UltimoIDEntrada,int tipoEntrada){
-        double importe,importeFinal;
-        Console.WriteLine("ingresa el importe de la primer entrada ");
-        importe = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("ingresa el importe de la entrada a cual vas a cambiar ");
-        importeFinal = double.Parse(Console.ReadLine());
-
-        if (importe < importeFinal)
+        public static int AgregarCliente(string nombre, string apellido, int dni, double fechaDeInscripcion, int tipoEntrada, int cantidad)
         {
-            return false;
+            Cliente clienteCompleto = new Cliente(dni, nombre, apellido, fechaDeInscripcion, tipoEntrada, cantidad);
+            DicCliente.Add(UltimoIDEntrada, clienteCompleto);
+            return UltimoIDEntrada++;
         }
-        else 
+
+        public static Cliente BuscarCliente(int idCliente)
         {
-            return true;
+            if (DicCliente.ContainsKey(idCliente))
+            {
+                return DicCliente[idCliente];
+            }
+            return null;
         }
-    }
-    List<string> estadisticasDelEvento = new List<string>();
-    public static void EstadisticasDelEvento(string nombre, string apellido,int dni,double fechaDeInscripcion,int tipoEntrada, int cantidad){
-         Cliente clienteCompleto = new Cliente(dni, nombre, apellido, fechaDeInscripcion,tipoEntrada,cantidad);
-         Console.WriteLine( "La cant de clientes que se inscribieron fueron " + clienteCompleto);
+
+        public static bool CambiarEntrada(Cliente cliente, int nuevoTipoEntrada)
+        {
+            double importe, importeFinal;
+            Console.WriteLine("Ingrese el importe de la entrada actual:");
+            importe = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el importe de la entrada a la que desea cambiar:");
+            importeFinal = double.Parse(Console.ReadLine());
+
+            if (importe < importeFinal)
+            {
+                return false;
+            }
+            else
+            {
+                cliente.TipoEntrada = nuevoTipoEntrada;
+                return true;
+            }
+        }
+
+        public static void EstadisticasDelEvento()
+        {
+            int cantidadClientesDia1 = 0, cantidadClientesDia2 = 0, cantidadClientesDia3 = 0, cantidadClientesFullPass = 0;
+            double importeTotal = 0;
+
+            foreach (var cliente in DicCliente.Values)
+            {
+                switch (cliente.TipoEntrada)
+                {
+                    case 1:
+                        cantidadClientesDia1++;
+                        importeTotal += cliente.Cantidad * 45000;
+                        break;
+                    case 2:
+                        cantidadClientesDia2++;
+                        importeTotal += cliente.Cantidad * 60000;
+                        break;
+                    case 3:
+                        cantidadClientesDia3++;
+                        importeTotal += cliente.Cantidad * 30000;
+                        break;
+                    case 4:
+                        cantidadClientesFullPass++;
+                        importeTotal += cliente.Cantidad * 100000;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Cantidad de clientes con entrada dia 1: {cantidadClientesDia1}");
+            Console.WriteLine($"Cantidad de clientes con entrada dia 2: {cantidadClientesDia2}");
+            Console.WriteLine($"Cantidad de clientes con entrada dia 3: {cantidadClientesDia3}");
+            Console.WriteLine($"Cantidad de clientes con entrada Full Pass: {cantidadClientesFullPass}");
+            Console.WriteLine($"Importe total recaudado: {importeTotal}");
+        }
     }
 
-}
  
     
    
